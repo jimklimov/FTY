108fb0ca37ceb6f81c9e149a5f6fb27ef3c5e4f8
Jim Klimov <jim@jimklimov.com>
2017-01-15 16:56:33 +0100

    Initial commit

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@
 # Initially fetch or subsequently update the components
 # referred to as "git submodule"'s
 
+git pull --all
 git submodule sync --recursive && \
 git submodule update --recursive && \
 git status -s
