8be77f9452a29d448b571cf449b11b023e4b594c
Jim Klimov <jim@jimklimov.com>
2017-01-15 17:57:26 +0100

    Initial commit

--- sync.sh
+++ sync.sh
@@ -3,8 +3,17 @@
 # Initially fetch or subsequently update the components
 # referred to as "git submodule"'s
 
+# See also
+# https://git-scm.com/book/en/v2/Git-Tools-Submodules
+# http://stackoverflow.com/questions/5828324/update-git-submodule-to-latest-commit-on-origin
+# http://stackoverflow.com/questions/1979167/git-submodule-update/1979194#1979194
+
+# Update dispatcher repo
 git pull --all
+
+# Update component repos
+# git submodule init --recursive && \
+git submodule update --recursive --remote --merge && \
 git submodule sync --recursive && \
-git submodule update --recursive && \
 git status -s
 
