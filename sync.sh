7292a6b7b7f9f43057cc9126c52f33830a542c44
Jim Klimov <jim@jimklimov.com>
2017-01-30 09:28:45 +0100

    First shot at Travis CI builds for FTY overall

--- sync.sh
+++ sync.sh
@@ -9,8 +9,28 @@
 # http://stackoverflow.com/questions/5828324/update-git-submodule-to-latest-commit-on-origin
 # http://stackoverflow.com/questions/1979167/git-submodule-update/1979194#1979194
 
+set -e
+
+# Set this to enable verbose profiling
+[ -n "${CI_TIME-}" ] || CI_TIME=""
+case "$CI_TIME" in
+    [Yy][Ee][Ss]|[Oo][Nn]|[Tt][Rr][Uu][Ee])
+        CI_TIME="time -p " ;;
+    [Nn][Oo]|[Oo][Ff][Ff]|[Ff][Aa][Ll][Ss][Ee])
+        CI_TIME="" ;;
+esac
+
+# Set this to enable verbose tracing
+[ -n "${CI_TRACE-}" ] || CI_TRACE="no"
+case "$CI_TRACE" in
+    [Nn][Oo]|[Oo][Ff][Ff]|[Ff][Aa][Ll][Ss][Ee])
+        set +x ;;
+    [Yy][Ee][Ss]|[Oo][Nn]|[Tt][Rr][Uu][Ee])
+        set -x ;;
+esac
+
 # Update dispatcher repo
-git pull --all
+$CI_TIME git pull --all
 
 # Update component repos
 # NOTE: sync is toxic to established workspaces, as it "resyncs the URL" and
@@ -18,11 +38,12 @@ git pull --all
 # fork) back to the upstream project URL. For daily usage, "update" suffices.
 # git submodule init --recursive && \
 # git submodule sync --recursive && \
-git submodule init && \
-git submodule foreach "git pull --all" && \
-git submodule update --recursive --remote --merge && \
-git status -s
+$CI_TIME git submodule init && \
+$CI_TIME git submodule foreach "git pull --all" && \
+$CI_TIME git submodule update --recursive --remote --merge && \
+$CI_TIME git status -s
 
+[ x"${DO_BUMP-}" = xno ] || \
 git status -s | while read STATUS OBJNAME ; do
     DO_BUMP=no
     if [ -n "$OBJNAME" ]; then
@@ -32,7 +53,7 @@ git status -s | while read STATUS OBJNAME ; do
     fi
 done
 
-if [ "$DO_BUMP" = yes ]; then
+if [ x"${DO_BUMP-}" = xyes ]; then
     echo "Adding changed objects to git commit..."
-    git commit -a -m 'Updated references to git submodule HEADs at '"`date -u`"
+    $CI_TIME git commit -a -m 'Updated references to git submodule HEADs at '"`date -u`"
 fi
